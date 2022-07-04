2025e76bad2d6a4b2f65ac026123591b7c1eedbc
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-04 14:37:06 -0600

    Code challenge 7-7 first attempt

--- CC-2022-7-7/app.js
+++ CC-2022-7-7/app.js
@@ -5,7 +5,7 @@
 
 // first attempt 
 
-// p(parameters): 
+// p(parameters): function that takes in that takes in an array.Need to consider caps or non caps. 
 // r(return):return either "Even" for even numbers or "Odd" for an odd number
 // e(example):
 [true,  true,  true,  false,
@@ -15,7 +15,20 @@
   true,  true,  true,  true ,
   false, false, true,  true]
   Correct Answer:17 
-// p():
+// p():write a function that takes in array. the function needs to cycle through the array and process what's in the array. if a true appears the function counts that word. If the word is not true than the function skips that word. THen the function will add all the trues and will count all the trues. 
 
+function countSheeps(arrayOfSheep){
+// let sheep = 0 
+let numberOfSheep.forEach(e =>{
+  if (arrayOfSheep === 'true'|| 'True'){
+    arrayOfSheep.reduce(p,c) => p + c, 0
+  }else{
+    continue
+  }
+  
+  }
+}
+
+// Did not succeed. I wasn't too positive on the whether to use "for each" or "reduce" or "if and else" or whether I should you all the above.
 
 
