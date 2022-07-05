d5ee299677cbe3b568c5ef463754463421e3f11b
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-05 13:52:53 -0600

    Code challenge 7-5

---
+++ CC-2022-5-7/app.js
@@ -0,0 +1,23 @@
+// Description:
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+
+
+
+
+// first attempt 
+
+// p(parameters): 
+// r(return):Finding the opposite of a given integer or a floating number. 
+// e(example):1:-1
+// e: 14:-14
+// e: -34:34
+// p():Function takes in integer or floating number and returns the opposite. 
+
+function opposite (number){ 
+
+    return (-number); 
+}
+
+
+
