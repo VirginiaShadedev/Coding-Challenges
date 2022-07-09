232664eb700fbc04c0cf934823278eca6e2bd64b
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-09 14:20:54 -0600

    adding code challenge to 7-9

--- CC-2022-8-7/app.js
+++ CC-2022-8-7/app.js
@@ -1,42 +1,25 @@
 // Description:
-//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//We need a function that can transform a number into a string. What ways of achieving this do you know?
 
 
 
-// first attempt 
 
-// p(parameters): function that takes in that takes in an array.Need to consider caps or non caps. 
-// r(return):return either "Even" for even numbers or "Odd" for an odd number
+// First attempt 
+
+// p(parameters): Function takes in number and spits out string. 
+// r(return):It will return a string
 // e(example):
-[true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
-  Correct Answer:17 
+// 123 --> "123"
+// 999 --> "999"
+// p(): Create a function numberToString(num) function will take in a number and will return a string. We will be using preset javascript .toString to achieve this function. 
 
-// p():write a function that takes in array. the function needs to cycle through the array and process what's in the array. if a true appears the function counts that word. If the word is not true than the function skips that word. THen the function will add all the trues and will count all the trues. 
-
-function countSheeps(arrayOfSheep){
-// let sheep = 0 
-let numberOfSheep.forEach(e =>{
-  if (arrayOfSheep === 'true'|| 'True'){
-    arrayOfSheep.reduce(p,c) => p + c, 0
-  }else{
-    continue
-  }
-  
-  }
+function numberToString(num){
+ return num.toString()
 }
 
-// Did not succeed. I wasn't too positive on the whether to use "for each" or "reduce" or "if and else" or whether I should you all the above.
+// notes on how you failed or succeeded. 
 
-// second Attempt 
-function countSheeps(arrayOfSheep){
-let sheep = arrayOfSheep.filter(sheep => sheep)
-return  sheep.length;
-}
+// The key to succeeding this code challenge is using .toString in order to take in number and return a string. 
 
-// After failing during the first attempt I came to understand that I was just thinking too much about it and that it was simplier than I thought. The two things that I used on this next attempt is filter. and .length . Filter actually filters through the array and looks for "sheep". The Length later counts the amount of times that sheep was used. using these both tools gave me a pass on the code Challenge. 
-// Even though I did get this right I was left with some questions of the syntax I used. I will look into it further tomorrow but my question is why does (sheep => sheep)? I changed it to (e => e) and that seemed to also give me a pass on the code challenge. 
+
+ 
