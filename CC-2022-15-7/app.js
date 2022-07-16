bb5e9d2b6a8719c6b4806bdf93f23a84176d3de4
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-15 19:46:18 -0600

    Adding second attempt to july 15

--- CC-2022-15-7/app.js
+++ CC-2022-15-7/app.js
@@ -19,10 +19,30 @@ const stringToNumber = function (str){
 }
 
 
-
 // notes on how you failed or succeeded. 
 
-// 
+// First attempt was a failure. The attempt was based on a previous code challenge that asked to create a function from number to string. The idea was that it would work the same but it failed.  
+
+// Second Attempt
+
+const stringToNumber = function(str){
+  
+  return Number(str)
+}
+// notes on how you failed or succeeded.
+// The attempt was to reverse the method that was attempted using number to string.The following method. 
+
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
+
+// I wasn't sure that 
+
+Number
+
+// was a thing, but alas it was. 
+
 
 // OTHER POSSIBLE SOLUTIONS 
 // Example 1 
