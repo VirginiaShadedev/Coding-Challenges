0b4512e59eea7b93424331780b3d7af6afa3418f
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-19 14:59:02 -0600

    adding a code challenge attempt to july 19

--- CC-2022-19-7/app.js
+++ CC-2022-19-7/app.js
@@ -7,15 +7,22 @@
 
 // First attempt 
 
-// p(parameters): 
-// r(return):
+// p(parameters): Create a function that reverses what it takes in. 
+// r(return):returns the word in reverse. 
 // e(example):
   // 'world'  =>  'dlrow'
   // 'word'   =>  'drow'
-// p():  
+// p():  I will create a function that reverses what is inside a string.
+
+
+function solution(str){
+  return reverse(str)
+}
+
 
 
 // notes on how you failed or succeeded. 
+// This was a failure. I wasn't sure if reverse was an actual way or valid in JS, but I still tried it. 
 
  
 
