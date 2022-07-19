c02b779d5dbcc6d088ed3dd1e21c52544e1a9bcc
Virginia Gutierrez <virginia07.gutierrez@gmail.com>
2022-07-19 14:31:26 -0600

    adding a new coding challenge for July 19

---
+++ CC-2022-19-7/app.js
@@ -0,0 +1,33 @@
+// Description:
+
+//Complete the solution so that it reverses the string passed into it.
+
+
+
+
+// First attempt 
+
+// p(parameters): 
+// r(return):
+// e(example):
+  // 'world'  =>  'dlrow'
+  // 'word'   =>  'drow'
+// p():  
+
+
+// notes on how you failed or succeeded. 
+
+ 
+
+// Second Attempt
+
+// notes on how you failed or succeeded.
+
+// OTHER POSSIBLE SOLUTIONS 
+// Example 1 
+
+// Example 2
+
+
+
+  
